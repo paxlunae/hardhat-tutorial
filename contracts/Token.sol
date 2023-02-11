5211166cd76993a4a73d7cf305c9b15ba3a29cf9
aman.steinberg <aman.steinberg@procon-it.de>
2023-02-11 15:51:56 +0100

    deployed to goerli testnet

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -46,6 +47,13 @@ contract Token {
         // transaction will revert.
         // TODO: how is the above-mentioned revert implemented?
         require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        console.log(
+        "Transferring from %s to %s %s tokens",
+        msg.sender,
+        to,
+        amount
+        );
 
         // Transfer the amount.
         balances[msg.sender] -= amount;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const ALCHEMY_API_KEY = "r5kZB1cH0NvUC7mFmYiqSV--foCrC-TQ";
+
+const GOERLI_PRIVATE_KEY = "69c3c294c026d1c80bfb9e418e50cf0ecb1f714d85bc8304d26dd024fdc5ccbe";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
+
